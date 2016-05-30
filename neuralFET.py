0e49d609b06e8a0699c24af673304096b76a8902
Oscarlight <oscarli2716@gmail.com>
2016-05-30 02:14:44 -0400

    0529 Version

--- neuralFET.py
+++ neuralFET.py
@@ -1,7 +1,6 @@
 ## ANN compact model by Mingda Li
 
 from ffnn import *
-from Current import *
 # --------------------------------------- 
 #   	       sig model
 # The first argument is the 1st quadrant model,
@@ -20,7 +19,7 @@ sig_ann = make_sig('sig_1q_7/', 'sig_3q_5/', 2)
 tan_ann = make_tan('tan_2h/', 3)
 
 # plot
-x_list = np.linspace(0.0, 0.4, 100)
+x_list = np.linspace(-0.1, 0.4, 100)
 c_list = [i*0.01 for i in range(0, 41)]
 sig_save = plot(sig_ann, x_list, c_list, TYPE = 'f', LOG = False)
 tan_save = plot(tan_ann, x_list, c_list, TYPE = 'f', LOG = False)
